3e15a460451b6d7090eecbf782d577ef5e692c33
Chandrashekar263 <m.saichandu53@gmail.com>
2025-01-08 17:35:16 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
 Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
-Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
-Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
-Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
-Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
-Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
 Console.ReadKey();
