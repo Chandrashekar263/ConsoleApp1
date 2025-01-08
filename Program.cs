562825bf28c7ef6f050e1fbd6c9bfc7ed3dbd216
Chandrashekar M <mchandrashekar983@gmail.com>
2025-01-08 17:10:56 +0530

    Responce

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Godavarikhani how are you");
+Console.WriteLine("Hello Godavarikhani how are you am fine what about you");
 Console.ReadKey();
