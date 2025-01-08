3aa4c3cf8d9d35aa70b959cbf39c1332afc305d9
Chandrashekar263 <m.saichandu53@gmail.com>
2025-01-08 16:58:02 +0530

    GDK

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello chandrashekar how r  where are you how r  where are youhow r  where are youhow r  where are youhow r  where are you" +
-    "how r  where are youhow r  where are youhow r  where are yo");
+Console.WriteLine("Hello Godavarikhani how are you");
 Console.ReadKey();
