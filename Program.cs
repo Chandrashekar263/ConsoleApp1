f33e6d3c48fe36398b36ce3e0d8fdf75e95d04ab
Chandrashekar M <mchandrashekar983@gmail.com>
2025-01-08 17:19:11 +0530

    ANIMALS

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("APPLE BALL CAT DOG");
+Console.WriteLine("APPLE BALL CAT DOG ELEPHANT GERAFI GOAT");
 Console.ReadKey();
